a5e8f8ef87ed0a8f93858dfe06264eabd06b75d3
marketing phonna <marketing.phonna@gmail.com>
2024-10-17 14:12:40 +0700

    first commit

---
+++ database/dbbukutamu.sql
@@ -0,0 +1,96 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 17 Okt 2024 pada 09.08
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dbbukutamu`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `ttamu`
+--
+
+CREATE TABLE `ttamu` (
+  `id` int(3) NOT NULL,
+  `tanggal` date NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `instansi` varchar(100) NOT NULL,
+  `alamat` varchar(200) NOT NULL,
+  `tujuan` varchar(100) NOT NULL,
+  `nope` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tuser`
+--
+
+CREATE TABLE `tuser` (
+  `id_user` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(128) NOT NULL,
+  `nama_pengguna` varchar(50) NOT NULL,
+  `status` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `tuser`
+--
+
+INSERT INTO `tuser` (`id_user`, `username`, `password`, `nama_pengguna`, `status`) VALUES
+(1, 'admin', '0192023a7bbd73250516f069df18b500', 'Admin', 'Aktif');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `ttamu`
+--
+ALTER TABLE `ttamu`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `tuser`
+--
+ALTER TABLE `tuser`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `ttamu`
+--
+ALTER TABLE `ttamu`
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
+--
+-- AUTO_INCREMENT untuk tabel `tuser`
+--
+ALTER TABLE `tuser`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
